123b100a3005704c76b8d58247dccf45e7b05937
Dominic Cerquetti <binary1230@gmail.com>
2018-06-16 23:19:03 -0400

    upadting for new build WIP

--- flightcomputer.lua
+++ flightcomputer.lua
@@ -1,14 +1,14 @@
 -- tweakable global vars
-livestream_server = "rtmp://dev.magfest.net/live"
-livestream_name = "test"
+-- livestream_server = "rtmp://dev.magfest.net/live"
+-- livestream_name = "test"
 
-livestream_url = "http://demo.splitmedialabs.com/VHJavaMediaSDK3/view.html?id=" .. livestream_name .. "&url=" .. livestream_server .. "&buffer=0&forceObjectEncoding=0"
+-- livestream_url = "http://demo.splitmedialabs.com/VHJavaMediaSDK3/view.html?id=" .. livestream_name .. "&url=" .. livestream_server .. "&buffer=0&forceObjectEncoding=0"
 -- livestream_url = "http://output.jsbin.com/vofoyoj/1" -- video.js experimental
 -- rest of code below
 
-webstream = peripheral.wrap("WebScreen_1")
-monitor = peripheral.wrap("right")
-telem_monitors = {"monitor_3", "monitor_4"}
+-- webstream = peripheral.wrap("WebScreen_1")
+monitor = peripheral.wrap("monitor_0")
+telem_monitors = {}    -- {"monitor_3", "monitor_4"}
 
 if monitor then
 	monitor.setTextScale(1)
@@ -16,12 +16,16 @@ if monitor then
 	term.clear()
 end
 
-os.loadAPI("ksp/flight/flight")
+print("starting...")
+
+print("loading API...")
+os.loadAPI("flight/flight")
+print("API loaded, continuing....")
 
 function init_webscreen()
 	print("resetting display")
 	print("resetting display to: " .. livestream_url)
-	webstream.setUrl(livestream_url)
+	-- webstream.setUrl(livestream_url)
 end
 
 function restart_computer()
@@ -68,32 +72,34 @@ roll_amount = 0.1
 yaw_amount = 0.5
 
 ksp_init_data = {
-	ksp_server = "http://10.0.0.29:8085", 
+	-- ksp_server = "http://10.0.0.29:8085", 
+	-- ksp_server = "http://127.0.0.1:8085", 
+	ksp_server = "http://10.0.0.16:8085", 
 	
 	render_telem=render_telem,
 	
 	-- description, color, attitude change
 	attitude_entries = {
-		{desc="pitch+", 	side="left", color=colors.orange, 	vector=flight.build_ksp_vector6(pitch_amount, 0, 0)},
-		{desc="pitch-", 	side="left", color=colors.red, 		vector=flight.build_ksp_vector6(-pitch_amount, 0, 0)},
-		{desc="yaw+", 		side="left", color=colors.pink, 		vector=flight.build_ksp_vector6(0, yaw_amount, 0)},
-		{desc="yaw-", 		side="left", color=colors.white, 	vector=flight.build_ksp_vector6(0, -yaw_amount, 0)},
-		{desc="roll-", 		side="left", color=colors.lightBlue,	vector=flight.build_ksp_vector6(0, 0, -roll_amount)},
-		{desc="roll+", 		side="left", color=colors.lime, 		vector=flight.build_ksp_vector6(0, 0, roll_amount)},
+		{desc="pitch+", 	side="left", color=colors.green, 		vector=flight.build_ksp_vector6(-pitch_amount, 0, 0)},
+		{desc="pitch-", 	side="left", color=colors.red, 			vector=flight.build_ksp_vector6(pitch_amount, 0, 0)},
+		{desc="yaw+", 		side="left", color=colors.brown, 		vector=flight.build_ksp_vector6(0, yaw_amount, 0)},
+		{desc="yaw-", 		side="left", color=colors.blue, 		vector=flight.build_ksp_vector6(0, -yaw_amount, 0)},
+		{desc="roll-", 		side="left", color=colors.lightGray,	vector=flight.build_ksp_vector6(0, 0, roll_amount)},
+		{desc="roll+", 		side="left", color=colors.lime, 		vector=flight.build_ksp_vector6(0, 0, -roll_amount)},
 	},
 
 	-- note: in Telemachus release as of 5/26/2016, staging is broken. use action group 1 instead.
 	toggle_entries = {
-		{desc="throttle", side="left", color=colors.black, offcmd="f.throttleZero", oncmd="f.throttleFull"},
-		{desc="rcs", side="left", color=colors.cyan, oncmd="f.rcs[true]", offcmd="f.rcs[false]"},
-		{desc="sas", side="left", color=colors.blue, oncmd="f.sas[true]", offcmd="f.sas[false]"},
-		{desc="stage", side="left", color=colors.brown, oncmd="f.stage", offcmd=nil},
+		{desc="throttle", side="left", color=colors.pink, offcmd="f.throttleZero", oncmd="f.throttleFull"},
+		{desc="rcs", side="left", color=colors.gray, oncmd="f.rcs[true]", offcmd="f.rcs[false]"},
+		{desc="sas", side="left", color=colors.magenta, oncmd="f.sas[true]", offcmd="f.sas[false]"},
+		{desc="stage", side="left", color=colors.white, oncmd="f.stage", offcmd=nil},
 		{desc="gear", side="left", color=colors.yellow, oncmd="f.gear", offcmd=nil},
-		{desc="light", side="left", color=colors.green, oncmd="f.light", offcmd=nil},
-		{desc="timewarp", side="left", color=colors.lightGray, oncmd="t.timeWarp[3]", offcmd="t.timeWarp[0]"},
+		{desc="light", side="left", color=colors.lightBlue, oncmd="f.light", offcmd=nil},
+		{desc="timewarp", side="left", color=colors.orange, oncmd="t.timeWarp[3]", offcmd="t.timeWarp[0]"},
 		
 		{desc="restart_computer", side="bottom", color=colors.yellow, callback=restart_computer},
-		{desc="restart_webstream", side="bottom", color=colors.blue, callback=init_webscreen},
+		-- {desc="restart_webstream", side="bottom", color=colors.blue, callback=init_webscreen},
 	},
 
 	-- things we will query FROM kerbal space program
